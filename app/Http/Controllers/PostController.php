b62e005a4308a2fdb9f8e8b8c47341f4653edf59
mudblood97 <ivanharsono111@gmail.com>
2022-05-08 23:55:50 +0700

    After Search & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,22 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::latest();
+        $title = '';
 
-        if(request('search')){
-            $posts->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
+        if(request('category')){
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
         }
+
+        if(request('author')){
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             'active' => 'posts',
-            "posts" => $posts->get()
+            "posts" =>  Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
